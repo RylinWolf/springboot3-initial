a363767bed595f650bcd9e2f5c9ba72c6791e223
RylinWolf <linexwolf@icloud.com>
2025-11-04 17:39:56 +0800

    feat: 新增 Spring Security 配置信息

---
+++ src/main/java/com/wolfhouse/springboot3initial/config/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.wolfhouse.springboot3initial.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Rylin Wolf
+ */
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(a -> {
+                a.requestMatchers(SecurityConstant.WHITELIST)
+                 .permitAll();
+                a.anyRequest()
+                 .authenticated();
+            });
+        return http.build();
+    }
+}
---
+++ src/main/java/com/wolfhouse/springboot3initial/config/security/SecurityConstant.java
@@ -0,0 +1,11 @@
+package com.wolfhouse.springboot3initial.config.security;
+
+/**
+ * @author Rylin Wolf
+ */
+public class SecurityConstant {
+    public static final String[] WHITELIST = {
+        "/v3/api-docs",
+        "/*"
+    };
+}
