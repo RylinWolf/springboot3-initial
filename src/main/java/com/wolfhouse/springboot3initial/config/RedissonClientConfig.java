5d872032de8fe3b992c07cf5c01ac2f78a56548f
RylinWolf <linexwolf@icloud.com>
2025-11-04 16:29:08 +0800

    feat: 新增 Redisson 配置类

---
+++ src/main/java/com/wolfhouse/springboot3initial/config/RedissonClientConfig.java
@@ -0,0 +1,36 @@
+package com.wolfhouse.springboot3initial.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson 客户端配置类
+ *
+ * @author Rylin Wolf
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+@Data
+public class RedissonClientConfig {
+    private Integer port;
+    private String password;
+    private String username;
+    private Integer database;
+    private String host;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+              .setAddress(String.format("redis://%s:%d", host, port))
+              .setDatabase(database)
+              .setUsername(username)
+              .setPassword(password);
+        return Redisson.create(config);
+    }
+}
