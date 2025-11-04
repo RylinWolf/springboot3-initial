412536a843d09bdaa6a5d4a4f6ea51c4054e79a3
RylinWolf <linexwolf@icloud.com>
2025-11-04 14:44:03 +0800

    build: 初始化提交

---
+++ src/main/java/com/wolfhouse/springboot3initial/Springboot3InitialApplication.java
@@ -0,0 +1,16 @@
+package com.wolfhouse.springboot3initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rylin Wolf
+ */
+@SpringBootApplication
+public class Springboot3InitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3InitialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wolfhouse/springboot3initial/Springboot3InitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfhouse.springboot3initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3InitialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
