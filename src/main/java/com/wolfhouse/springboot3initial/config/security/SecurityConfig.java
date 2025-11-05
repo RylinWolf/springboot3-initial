3fd698d61782417b0d5ed6924636b336e1c9f964
RylinWolf <linexwolf@icloud.com>
2025-11-05 11:48:06 +0800

    feat: 新增密码编码器 Bean 配置

--- src/main/java/com/wolfhouse/springboot3initial/config/security/SecurityConfig.java
+++ src/main/java/com/wolfhouse/springboot3initial/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -24,5 +25,10 @@ public class SecurityConfig {
                  .authenticated();
             });
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
