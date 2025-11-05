126d286c40fedf5fbedb42e7ced3a586b89128b8
RylinWolf <linexwolf@icloud.com>
2025-11-05 14:18:40 +0800

    feat: 为异常工具类新增根据条件抛出异常的方法及其重载

--- src/main/java/com/wolfhouse/springboot3initial/common/util/beanutil/ThrowUtil.java
+++ src/main/java/com/wolfhouse/springboot3initial/common/util/beanutil/ThrowUtil.java
@@ -8,12 +8,18 @@ import java.lang.reflect.InvocationTargetException;
  * @author Rylin Wolf
  */
 public class ThrowUtil {
+    public static Class<? extends Throwable> exceptionClazz = BeanUtilException.class;
+
     public static void throwIfBlank(Object o) {
-        throwIfBlank(o, BeanUtilException.class);
+        throwIfBlank(o, exceptionClazz);
     }
 
     public static void throwIfBlank(Object o, String msg) {
-        throwIfBlank(o, msg, BeanUtilException.class);
+        throwIfBlank(o, msg, exceptionClazz);
+    }
+
+    public static void throwIfBlank(Object o, Integer code, String msg) {
+        throwIfBlank(o, code, msg, exceptionClazz);
     }
 
     public static void throwIfBlank(Object o, Class<? extends Throwable> clazz) {
@@ -30,6 +36,55 @@ public class ThrowUtil {
                      InvocationTargetException e) {
                 throw new BeanUtilException(e);
             }
+        }
+    }
+
+    @SneakyThrows
+    public static void throwIfBlank(Object o, Integer code, String msg, Class<? extends Throwable> clazz) {
+        if (BeanUtil.isBlank(o)) {
+            try {
+                throw clazz.getConstructor(Integer.class, String.class)
+                           .newInstance(code, msg);
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                     InvocationTargetException e) {
+                throw new BeanUtilException(e);
+            }
+        }
+    }
+
+    public static void throwOnCondition(boolean b, String msg) {
+        throwOnCondition(b, msg, exceptionClazz);
+    }
+
+    public static void throwOnCondition(boolean b, Integer code, String msg) {
+        throwOnCondition(b, code, msg, exceptionClazz);
+    }
+
+    @SneakyThrows
+    public static void throwOnCondition(boolean b, String msg, Class<? extends Throwable> clazz) {
+        if (!b) {
+            return;
+        }
+        try {
+            throw clazz.getConstructor(String.class)
+                       .newInstance(msg);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            throw new BeanUtilException(e);
+        }
+    }
+
+    @SneakyThrows
+    public static void throwOnCondition(boolean b, Integer code, String msg, Class<? extends Throwable> clazz) {
+        if (!b) {
+            return;
+        }
+        try {
+            throw clazz.getConstructor(Integer.class, String.class)
+                       .newInstance(code, msg);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            throw new BeanUtilException(e);
         }
     }
 }
