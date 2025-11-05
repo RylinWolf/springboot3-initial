2904580f7466e0b5cf9c68bd5805e3c87de4216f
RylinWolf <linexwolf@icloud.com>
2025-11-05 14:20:05 +0800

    feat: 修改自定义服务异常类，为其新增错误代号字段

--- src/main/java/com/wolfhouse/springboot3initial/exception/ServiceException.java
+++ src/main/java/com/wolfhouse/springboot3initial/exception/ServiceException.java
@@ -1,14 +1,24 @@
 package com.wolfhouse.springboot3initial.exception;
 
+import lombok.Getter;
+
 /**
  * 自定义服务异常类
  *
  * @author Rylin Wolf
  */
+@Getter
 public class ServiceException extends RuntimeException {
+    private Integer code = 500;
+
     public ServiceException() {}
 
     public ServiceException(String message) {
         super(message);
+    }
+
+    public ServiceException(Integer code, String message) {
+        super(message);
+        this.code = code;
     }
 }
