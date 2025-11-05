492b247244b70ff7b16e4ad81aac969aac17517e
RylinWolf <linexwolf@icloud.com>
2025-11-05 14:52:01 +0800

    feat: 新增全局异常处理器，提供 SQL 异常处理器

---
+++ src/main/java/com/wolfhouse/springboot3initial/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.wolfhouse.springboot3initial.handler;
+
+import com.wolfhouse.springboot3initial.common.result.HttpCode;
+import com.wolfhouse.springboot3initial.common.result.HttpResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLSyntaxErrorException;
+
+/**
+ * 全局异常处理器
+ *
+ * @author Rylin Wolf
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<HttpResult<?>> sqlException(SQLSyntaxErrorException e) {
+        log.error("SQL 异常: {}", e.getMessage(), e);
+        return HttpResult.failedWithStatus(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                           HttpCode.SQL_ERROR);
+    }
+}
