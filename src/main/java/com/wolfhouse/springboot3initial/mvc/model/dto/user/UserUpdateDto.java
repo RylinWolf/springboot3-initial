74baccf1508d4a4147a6982f9c5303aa5ac530df
RylinWolf <linexwolf@icloud.com>
2025-11-05 17:29:04 +0800

    feat: 新增用户更新 Dto

---
+++ src/main/java/com/wolfhouse/springboot3initial/mvc/model/dto/user/UserUpdateDto.java
@@ -0,0 +1,48 @@
+package com.wolfhouse.springboot3initial.mvc.model.dto.user;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * @author Rylin Wolf
+ */
+@Data
+public class UserUpdateDto {
+    /**
+     * 用户名称
+     */
+    private String username;
+
+    /**
+     * 头像
+     */
+    private String avatar;
+
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    /**
+     * 生日
+     */
+    private LocalDate birth;
+
+    /**
+     * 个性标签
+     */
+    private String personalStatus;
+
+    /**
+     * 手机
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    @Email
+    private String email;
+}
