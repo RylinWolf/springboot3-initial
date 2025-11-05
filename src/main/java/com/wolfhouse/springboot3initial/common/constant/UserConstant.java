03cd514cb4e66f97070555195e00ce330d7b200c
RylinWolf <linexwolf@icloud.com>
2025-11-05 14:20:24 +0800

    feat: 新增常量字段

---
+++ src/main/java/com/wolfhouse/springboot3initial/common/constant/UserConstant.java
@@ -0,0 +1,10 @@
+package com.wolfhouse.springboot3initial.common.constant;
+
+/**
+ * @author Rylin Wolf
+ */
+public class UserConstant {
+
+    public static final String ILLEGAL_BIRTH = "生日格式非法";
+    public static final String REGISTER_FAILED = "用户注册失败";
+}
--- src/main/java/com/wolfhouse/springboot3initial/common/util/verify/servicenode/ServiceVerifyConstant.java
+++ src/main/java/com/wolfhouse/springboot3initial/common/util/verify/servicenode/ServiceVerifyConstant.java
@@ -5,4 +5,5 @@ package com.wolfhouse.springboot3initial.common.util.verify.servicenode;
  */
 public interface ServiceVerifyConstant {
     String ILLEGAL_PASSWORD = "密码格式不合法";
+    String ILLEGAL_PHONE = "手机号格式不合法";
 }
