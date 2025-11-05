458a8101745ed596753ac6e45e90150a0b20d4d5
RylinWolf <linexwolf@icloud.com>
2025-11-05 16:45:19 +0800

    feat: 为用户查询 Dto 新增分页功能 fix: 修改基础分页类的分页字段类型

--- src/main/java/com/wolfhouse/springboot3initial/common/result/PageQueryDto.java
+++ src/main/java/com/wolfhouse/springboot3initial/common/result/PageQueryDto.java
@@ -1,13 +1,12 @@
 package com.wolfhouse.springboot3initial.common.result;
 
 import lombok.Data;
-import org.openapitools.jackson.nullable.JsonNullable;
 
 /**
  * @author Rylin Wolf
  */
 @Data
 public class PageQueryDto {
-    protected JsonNullable<Integer> pageNum = JsonNullable.of(1);
-    protected JsonNullable<Integer> pageSize = JsonNullable.of(10);
+    protected Long pageNum = 1L;
+    protected Long pageSize = 10L;
 }
--- src/main/java/com/wolfhouse/springboot3initial/mvc/model/dto/user/UserQueryDto.java
+++ src/main/java/com/wolfhouse/springboot3initial/mvc/model/dto/user/UserQueryDto.java
@@ -1,13 +1,16 @@
 package com.wolfhouse.springboot3initial.mvc.model.dto.user;
 
+import com.wolfhouse.springboot3initial.common.result.PageQueryDto;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 /**
  * @author Rylin Wolf
  */
 @Data
-public class UserQueryDto {
+@EqualsAndHashCode(callSuper = false)
+public class UserQueryDto extends PageQueryDto {
     private JsonNullable<String> username = JsonNullable.undefined();
     private JsonNullable<String> email = JsonNullable.undefined();
     private JsonNullable<String> account = JsonNullable.undefined();
